e78f10ccf04d560263915e60ad648aa2fab56aa3
luatoolz <luatoolz@somahorse.com>
2024-07-25 10:33:20 +0300

    fix: remove metatables

--- lua/t/format/json.lua
+++ lua/t/format/json.lua
@@ -4,12 +4,20 @@ local jsonlib = require "rapidjson"
 local options_pretty = {pretty=true, sort_keys=true, empty_table_as_array=true}
 local options_sort = {sort_keys=true}
 
+local function clear(self)
+  if type(self)~='table' then return self end
+  setmetatable(self, nil)
+  for k,v in pairs(self) do clear(v) end
+  return self
+end
+
 return t.object({
   pretty=function(x) return jsonlib.encode(x, options_pretty) end,
   encode=function(x) return jsonlib.encode(x, options_sort) end,
-  decode=jsonlib.decode,
+  decode=function(x) return clear(jsonlib.decode(x)) end,
   __call=function(self, x)
-    if type(x)=='string' then return self.decode(x) end
-    if type(x)=='table' then return x end
+    if type(x)=='string' then x=self.decode(x); end
+    if type(x)=='table' then return clear(x) end
+    return x
   end,
 }):factory()
