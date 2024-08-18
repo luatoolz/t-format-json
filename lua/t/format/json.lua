4aedadbb5e9c701464cfcc45d9e47a8bea896dfd
luatoolz <luatoolz@somahorse.com>
2024-08-19 00:13:21 +0300

    refactoring as object

--- lua/t/format/json.lua
+++ lua/t/format/json.lua
@@ -1,4 +1,3 @@
-local t = require "t"
 local jsonlib = require "rapidjson"
 local meta = require "meta"
 local no = meta.no
@@ -7,6 +6,13 @@ local ngx=ngx or {}
 local options_pretty = {pretty=true, sort_keys=true, empty_table_as_array=true}
 local options_sort = {sort_keys=true, empty_table_as_array=true}
 
+local getmetatable = debug.getmetatable or getmetatable
+
+local atom = {
+  ["number"]=true,
+  ["boolean"]=true,
+}
+
 local function clear(self)
   if type(self)~='table' then return self end
   setmetatable(self, nil)
@@ -14,15 +20,55 @@ local function clear(self)
   return self
 end
 
-return t.object({
+local function __json(x)
+  local to = (getmetatable(x or {}) or {}).__tojson
+  if type(to)=='function' then return to(x) end
+end
+
+local function __string(x)
+  local to = (getmetatable(x or {}) or {}).__tostring
+  if type(to)=='function' then return to(x) end
+end
+
+local json = setmetatable({
   pretty=function(x) return jsonlib.encode(x, options_pretty) end,
-  encode=function(x) return assert(jsonlib.encode(clear(x), options_sort)) end,
-  decode=function(x) return clear(jsonlib.decode(x)) end,
-  __call=function(self, x)
-    if type(x)=='string' then return self.decode(x); end
-    if type(x)=='table' then
-      if getmetatable(x) and type(getmetatable(x).__tostring)=='function' then return tostring(x) end
-      if not getmetatable(x) then return assert(jsonlib.encode(clear(x), options_sort)) end end
-    return x
+  encode=function(x)
+    if type(x)=='string' then return x end
+    if type(x)~='table' and type(x)~='userdata' then return jsonlib.encode(x, options_sort) end
+    return assert(jsonlib.encode(clear(x), options_sort))
   end,
-}):factory()
+  decode=function(x) return clear(jsonlib.decode(x)) end,
+},{
+  __call=function(self, x, deep)
+    if atom[type(x)] and not deep then return self.encode(x) end
+    if type(x)=='number' then return x end
+    if type(x)=='boolean' then return x end
+    if type(x)=='string' then return x end
+    if type(x)=='nil' then return 'null' end
+    if type(x)=='function' then return nil end
+    if type(x)=='thread' then return nil end
+    if type(x)=='userdata' then
+      if type((getmetatable(x or {}) or {}).__tojson)=='function' then
+        x=__json(x) --or __string(x)
+        if deep then return x end
+        return type(x)=='string' and x or self.encode(x)
+      end
+      return 'userdata'
+    end
+    if type(x)=='table' then
+      if type((getmetatable(x or {}) or {}).__tojson)=='function' then
+        x=__json(x)
+        if deep then return x end
+        return self.encode(x)
+      end
+      for k,v in pairs(x) do x[k]=self(v, true) end
+      if deep then return x end
+      return self.encode(x)
+    end -- just convert
+    error('unknownn type' .. type(x))
+  end,
+})
+
+tojson = json
+
+return json
--- spec/json_spec.lua
+++ spec/json_spec.lua
@@ -5,6 +5,10 @@ describe("json", function()
     is = t.is
     json = require "t.format.json"
   end)
+	it("load", function()
+    assert.is_table(json)
+    assert.is_table(getmetatable(json))
+  end)
 	it("is", function()
     assert.is_false(is.json())
     assert.is_false(is.json(''))
@@ -37,9 +41,9 @@ describe("json", function()
 
 		assert.equal('7', json.encode(7))
 		assert.equal('7.2', json.encode(7.2))
-		assert.equal('"7"', json.encode("7"))
+		assert.equal('7', json.encode("7"))
 
-		assert.equal('""', json.encode(""))
+		assert.equal('', json.encode(""))
 
 		assert.equal('true', json.encode(true))
 		assert.equal('false', json.encode(false))
@@ -65,5 +69,18 @@ describe("json", function()
 
 		assert.equal('{\n    "a": 1\n}', json.pretty({a=1}))
 		assert.equal('{\n    "a": 1,\n    "b": "2"\n}', json.pretty({a=1,b="2"}))
+	end)
+	it("objects", function()
+		assert.equal('[]', json({}))
+		assert.equal("[1]", json({1}))
+		assert.equal("[1,2]", json({1,2}))
+		assert.equal('{"a":1}', json({a=1}))
+		assert.equal('{"a":1,"b":"2"}', json({a=1,b="2"}))
+
+		assert.equal('[]', json(t.array({})))
+		assert.equal('[1,2,3]', json(t.array({1,2,3})))
+
+		assert.equal('[]', json(table({})))
+		assert.equal('[1,2,3]', json(table({1,2,3})))
 	end)
 end)
